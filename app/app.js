95428b81b2bfa36dee256c6537b2895c14b1d5cb
Freeman <48633960+freemanlol@users.noreply.github.com>
2020-06-26 19:41:29 +0300

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const closeAndOpen = document.querySelector('.btn-burger-menu'),
+        navigationMobile = document.querySelector('.nav--mobile'),
+        serviceItems = document.querySelectorAll('.service__item'),
+        newsPost = document.querySelector('.section--post__inner'),
+        advantages = document.querySelector('.advantages'),
+        sectionReviews = document.querySelector('.section--reviews'),
+        newPost2 = document.querySelector('.section--post2'),
+        footerNav = document.querySelector('.footer__nav');
+    let c = 0;
+
+    closeAndOpen.addEventListener('click', () => {
+        c++;
+        navigationMobile.classList.remove("animate__backOutUp");
+        navigationMobile.style.display = "block";
+        navigationMobile.classList.add("animate__backInDown");
+        if (c == 2) {
+            navigationMobile.classList.add("animate__backOutUp");
+            setTimeout(function timeAnime() {
+                navigationMobile.style.display = "";
+            }, 1000);
+            c -= 2;
+        }
+    });
+
+    // width = 
+    // window
+    
+    if (document.body.clientWidth >= 590) {
+        console.log("я работаю")
+        window.addEventListener('scroll', function() {
+            if (pageYOffset >= 190) {
+                serviceItems.forEach(item => {
+                    item.classList.add("animate__backInRight");
+                })
+            }
+            if (pageYOffset >= 700) {
+                newsPost.classList.add("animate__backInRight");
+            }
+            if (pageYOffset >= 1200) {
+                advantages.classList.add("animate__backInRight");
+            }
+            if (pageYOffset >= 1800) {
+                sectionReviews.classList.add("animate__backInRight");
+            }
+            if (pageYOffset >= 2200) {
+                newPost2.classList.add("animate__backInRight");
+            }
+            if (pageYOffset >= 2700) {
+                footerNav.classList.add("animate__backInRight");
+            }
+        });
+    }  
+});
+
+
